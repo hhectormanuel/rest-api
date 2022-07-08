338d4f70b8f395ae7c5087c7c2c4d18ff9687160
Hector <hectorcena@live.com.mx>
2022-07-07 19:34:50 -0600

    precio agregado2

--- api/urls.py
+++ api/urls.py
@@ -2,12 +2,13 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from producto.api import get
+from producto.api import get, usuario_detail
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('producto.router')),
     path("get/", get),
+    path("delete/<int:pk>", usuario_detail)
     
 
 ]
--- producto/api.py
+++ producto/api.py
@@ -6,6 +6,7 @@ from django.http import HttpResponse
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.generics import RetrieveUpdateDestroyAPIView
 
 
 
@@ -23,7 +24,29 @@ def get(request):
             return Response(producto_serializer.data,status=status.HTTP_202_ACCEPTED)
         return Response(producto_serializer.errors)
 
+@api_view(['GET', 'PUT', 'DELETE'])
+def usuario_detail(request, pk=None):
+    usuario = Producto.objects.get(id = pk)
+    if usuario:
+        if request.method == 'GET':
+            
+            usuario_serializer = ProductoSerializer(usuario)
+            return Response(usuario_serializer.data, status=status.HTTP_200_OK)
+
+        elif request.method == 'PUT':
+            print('Entro al put')
+            usuario_serializer = ProductoSerializer(usuario, data=request.data)
+            if usuario_serializer.is_valid():
+                usuario_serializer.save()
+                return Response(usuario_serializer.data, status=status.HTTP_200_OK)
+            return Response(usuario_serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+
 
 class ProductoViewSet(ModelViewSet):
+    serializer_class = ProductoSerializer
+    queryset = serializer_class.Meta.model.objects.all()
+
+
+class Chupeto(RetrieveUpdateDestroyAPIView):
     serializer_class = ProductoSerializer
     queryset = serializer_class.Meta.model.objects.all()
